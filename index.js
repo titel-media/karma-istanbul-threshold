e9a12549e7d19cfb96141d72dfae1cc5a9942abb
Daniel Schaefer <epegzz@gmail.com>
2016-08-11 12:47:48 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const chalk = require('chalk');
+const _ = require('lodash');
+const fs = require('fs');
+const istanbul = require('istanbul');
+const checker = require('istanbul-threshold-checker');
+const checkCoverage = require('./lib').checkCoverage;
+const chokidar = require('chokidar');
+
+let reportFinished = function () { };
+
+var KarmaIstanbulThresholdReporter = function (baseReporterDecorator, config) {
+  // extend the base reporter
+  baseReporterDecorator(this);
+  var self = this;
+  self.skip = false;
+
+  config.remappedCoverageReporter = config.remappedCoverageReporter || {};
+  _.defaults(config.remappedCoverageReporter, {
+    src: null,
+    basePath: null,
+    thresholds: {
+      global: {
+        statements: 0,
+        branches: 0,
+        lines: 0,
+        functions: 0
+      },
+      each: {
+        statements: 0,
+        branches: 0,
+        lines: 0,
+        functions: 0
+      }
+    }
+  });
+
+  // disable chalk when colors is set to false
+  chalk.enabled = config.colors !== false;
+
+  self.parseResults = function () {
+    const json = JSON.parse(fs.readFileSync(config.remappedCoverageReporter.src, 'utf8'));
+    self.write('\n');
+    const exitCode = checkCoverage(
+      json,
+      config.remappedCoverageReporter.thresholds,
+      self.write.bind(self),
+      config.remappedCoverageReporter.basePath
+    );
+    self.write('\n');
+
+    if (exitCode) {
+      process.exit(exitCode);
+    }
+
+    // Reassign `onExit` to just call `done` since we are done here
+    this.onExit = function (done) {
+      done();
+    };
+
+    reportFinished();
+  };
+
+
+  let $results;
+  self.onRunComplete = function (browsers, results) {
+    $results = results;
+    self.write('aa...' + results);
+    if (results.exitCode) {
+      return;
+    }
+    if (!config.remappedCoverageReporter.src) {
+      self.write('\n' + chalk.reset('remapped-coverage: no src set.' +
+          ' Skipping coverage test') + '\n');
+    } else {
+      var watcher = chokidar.watch(config.remappedCoverageReporter.src);
+
+      watcher.on('add', (path, stats) => {
+        watcher.close();
+        self.parseResults();
+      });
+
+      setTimeout(function () {
+        self.write(chalk.red('\nWaiting for remapped coverage source timed out…\n'));
+        watcher.close();
+        reportFinished();
+      }, 5000);
+    }
+  };
+
+  self.onExit = function (done) {
+    reportFinished = done;
+  }
+};
+
+KarmaIstanbulThresholdReporter.$inject = ['baseReporterDecorator', 'config'];
+
+module.exports = {
+  'reporter:istanbul-threshold': ['type', KarmaIstanbulThresholdReporter]
+};
---
+++ lib.js
@@ -0,0 +1,139 @@
+'use strict';
+
+const chalk = require('chalk');
+const _ = require('lodash');
+const fs = require('fs');
+const istanbul = require('istanbul');
+const utils = require('istanbul/lib/object-utils');
+const path = require('path');
+
+const TYPES = ['lines', 'statements', 'functions', 'branches'];
+
+const defaultThresholds = {
+  global: {
+    statements: 0,
+      branches: 0,
+      lines: 0,
+      functions: 0,
+  },
+  each: {
+    statements: 0,
+      branches: 0,
+      lines: 0,
+      functions: 0,
+  }
+};
+
+const checkCoverage = function (
+  json, thresholds = defaultThresholds, log = console.log, basePath
+) {
+
+  let failed = false;
+
+  const checkThreshold = function(threshold, summary) {
+    var result = { failed: false };
+
+    // Check for no threshold
+    if (!threshold) {
+      result.skipped = true;
+      return result;
+    }
+
+    // Check percentage threshold
+    if (threshold > 0) {
+      result.value = summary.pct;
+      result.failed = result.value < threshold;
+    }
+    // Check gap threshold
+    else {
+      result.value = summary.covered - summary.total;
+      result.failed = result.value < threshold;
+    }
+
+    return result;
+  };
+
+  const checkThresholds = function(thresholds, summary) {
+    return TYPES.map(function(type) {
+      // If the threshold is a number use it, otherwise lookup the threshold type
+      var threshold = typeof thresholds === 'number' ? thresholds : thresholds && thresholds[type];
+      return checkThreshold(threshold, summary[type]);
+    });
+  };
+
+  const recordFailure = function (scope, type, value, filename) {
+    failed = true;
+    const expected = thresholds[scope][type];
+    if (filename) {
+      filename = path.relative(basePath, filename);
+    }
+    filename = scope === 'global' ? 'GLOBAL' : filename;
+
+    // make filename bold
+    if (filename) {
+      filename = filename.split('/');
+      filename[filename.length - 1] = chalk.yellow(_.last(filename));
+      filename = filename.join('/');
+    }
+
+
+    log(
+        chalk.red.bold('Low Coverage: ')
+      + filename + ' '
+      + chalk.bold(value + '%') + ' of '
+      + chalk.bold(expected + '% ')
+      + chalk.bold(type)
+      + '\n'
+    );
+  };
+
+  const checkFailures = function(thresholds, coverage) {
+    var summary = TYPES.map(function(type) {
+      return { type: type };
+    });
+
+    // If there are global thresholds check overall coverage
+    if (thresholds.global) {
+      var global = checkThresholds(thresholds.global, utils.summarizeCoverage(coverage));
+      // Inject into summary
+      summary.map(function(metric, i) {
+        metric.global = global[i];
+        if (global[i].failed) {
+          recordFailure('global', metric.type, metric.global.value);
+        }
+      });
+    }
+
+    // If there are individual thresholds check coverage per file
+    if (thresholds.each) {
+      _.each(coverage, function(fileCoverage, filename) {
+        // Check failures for a file
+        var each = checkThresholds(
+          thresholds.each,
+          utils.summarizeFileCoverage(fileCoverage)
+        );
+        _.map(each, function(item, i) {
+          if (item.failed) {
+            recordFailure('each', TYPES[i], item.value, filename);
+          }
+        });
+      });
+    }
+
+    return summary;
+  };
+
+  const collector = new istanbul.Collector();
+  collector.add(json);
+
+  const coverage = collector.getFinalCoverage();
+  checkFailures(thresholds, coverage);
+
+  collector.dispose();
+
+  return failed ? 1 : 0;
+};
+
+module.exports = {
+  checkCoverage
+};
